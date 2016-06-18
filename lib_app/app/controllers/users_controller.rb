e30c3781910870f1acdf34b202212611d558d80b
Michael Norelli <mnorelli360@gmail.com>
2016-06-18 13:04:35 -0700

    adds Session model and methods

--- lib_app/app/controllers/users_controller.rb
+++ lib_app/app/controllers/users_controller.rb
@@ -12,13 +12,20 @@ class UsersController < ApplicationController
 
   def create
     user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+    
     @user = User.new(user_params)
     if @user.save
-      redirect_to users
+      redirect_to users_path
     else
       render new_user
     end
   end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
 
   private
 

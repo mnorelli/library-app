472630426a7883bff2fa3c0a6700f352a6cf420c
Michael Norelli <mnorelli360@gmail.com>
2016-06-18 11:10:50 -0700

    adds new user route, contrller, and view

--- lib_app/app/controllers/users_controller.rb
+++ lib_app/app/controllers/users_controller.rb
@@ -5,5 +5,25 @@ class UsersController < ApplicationController
     render :index
   end 
 
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users
+    else
+      render new_user
+    end
+  end
+
+  private
+
+  def params_validate
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
 
 end
